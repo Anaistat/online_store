1257a101fd06d7ee6a4d5e9c63ce91f99eed899e
Anaistat <tanyazverevaa@gmail.com>
2022-07-14 17:20:52 +0300

    add Header component

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Header.module.sass'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
